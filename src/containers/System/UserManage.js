615a5d06e846b1736b3dfd6d34f9d15befe01fa4
luanpc <luanpc2805@gmail.com>
2022-01-15 04:35:07 +0700

    create & delete user

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,122 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManager.scss';
+import { getAllUsers, createNewUser, deleteUser } from '../../services/userService';
+import ModalUser from './ModalUser';
+import { emitter } from '../../utils/emitter';
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
+            isOpenModalUser: false
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        await this.getAllUsersFromReact();
     }
 
+    getAllUsersFromReact = async () => {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+    }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
+
+    createNewUser = async (data) => {
+        try {
+            let response = await createNewUser(data);
+            if (response && response.errCode !== 0) {
+                alert(response.errMessage)
+            } else {
+                await this.getAllUsersFromReact();
+                this.setState({
+                    isOpenModalUser: false
+                })
+                emitter.emit('EVENT_CLEAR_MODAL_DATA');
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    handleDeleteUser = async (user) => {
+        try {
+            let res = await deleteUser(user.id);
+            if (res && res.errCode === 0) {
+                await this.getAllUsersFromReact();
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className='users-container'>
+                <ModalUser isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    createNewUser={this.createNewUser}
+                />
+                <div className='title text-center'>MANAGE USERS</div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3'
+                        onClick={() => this.handleAddNewUser()}
+                    >
+                        <i className='fas fa-plus'></i>
+                        Add new user
+                    </button>
+                </div>
+                <div className='users-table mt-3 mx-1'>
+                    <table id="customers">
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Address</th>
+                                <th>Action</th>
+                            </tr>
+
+                            {arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit'><i className='fas fa-pencil-alt'></i></button>
+                                            <button className='btn-delete'
+                                                onClick={() => this.handleDeleteUser(item)}
+                                            ><i className='fas fa-trash'></i></button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,20 @@ const handleLoginApi = (email, password) => {
     return axios.post('/api/login', { email, password });
 }
 
-export { handleLoginApi }
+const getAllUsers = (id) => {
+    return axios.get(`/api/get-all-users?id=${id}`);
+}
+
+const createNewUser = (data) => {
+    return axios.post('/api/create-new-user', data)
+}
+
+const deleteUser = (userId) => {
+    return axios.delete('/api/delete-user', { data: { id: userId } })
+}
+export {
+    handleLoginApi,
+    getAllUsers,
+    createNewUser,
+    deleteUser
+}
