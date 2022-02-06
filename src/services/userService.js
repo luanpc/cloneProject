c03d82c311cb48e7e0452cd33888d36455a216da
luanpc <luanpc2805@gmail.com>
2022-02-06 13:20:48 +0700

    design create specialty component

--- src/services/userService.js
+++ src/services/userService.js
@@ -63,6 +63,10 @@ const postPatientBookAppointment = (data) => {
 const postVerifyBookAppointment = (data) => {
     return axios.post('/api/verify-book-appointment', data);
 }
+
+const createNewSpecialty = (data) => {
+    return axios.post('/api/create-new-specialty', data);
+}
 export {
     handleLoginApi,
     getAllUsers,
@@ -79,5 +83,6 @@ export {
     getExtraInfoDoctorById,
     getProfileDoctorById,
     postPatientBookAppointment,
-    postVerifyBookAppointment
+    postVerifyBookAppointment,
+    createNewSpecialty
 }
